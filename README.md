7340b9638be844f2784bf341456e8a9bbd6f5617
Richard Shea <rshea@thecubagroup.com>
2015-01-06 15:27:06 +1300

    Now produces a blank canvas from example1.html

--- README.md
+++ README.md
@@ -2,3 +2,18 @@ geescale
 ========
 
 A music theory game/teaching tool. Experimental. Currently based on Quintus.
+
+A couple of reminders
+========
+Serving
+-------
+```
+python -m SimpleHTTPServer
+```
+
+Initial Code
+-------------
+The initial few revs are based on this tutorial
+```
+http://www.remcodraijer.nl/quintus/tutorial.html
+```
---
+++ gs/js/example1.js
@@ -0,0 +1,13 @@
+window.addEventListener("load",function() { // Wait for the window to finish loading
+ 
+var Q = window.Q = Quintus()                // Create a new engine instance
+    .include("Sprites, Scenes, Input, 2D, Anim, Touch, UI") // Load any needed modules
+    .setup("myGame")                        // Bind Quintus to the canvas with ID "myGame"
+    .controls()                             // Add in default controls (keyboard, buttons)
+    .touch();                               // Add in touch support (for the UI)
+     
+    /*
+    ... Actual game code goes here ...
+    */
+     
+});
